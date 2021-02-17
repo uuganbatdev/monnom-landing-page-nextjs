927b5d1b4e6d3921fb148c886c5f33f7a45e3fb9
uuganbat gantumur <kite@pop-os.localdomain>
2021-02-17 22:18:05 +0800

    added compressed pictures

--- components/heads.js
+++ components/heads.js
@@ -4,7 +4,8 @@ import Head from 'next/head'
 function Heads() {
 	return(
 		<Head>
-			<title>MON NOM</title>
+			<title>MON|NOM</title>
+			<link rel='icon' href='/logo.png' type='image' />
 			<link rel="preconnect" href="https://fonts.gstatic.com" />
 			<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
 		</Head>
